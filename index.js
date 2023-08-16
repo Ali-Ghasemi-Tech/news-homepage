49f288033ba333ac0507bd978d8cc36d24d0ba99
TLB30 <thelastblade30@gmail.com>
2023-08-16 19:33:07 +0330

    add smooth scroll for links

--- index.js
+++ index.js
@@ -13,3 +13,14 @@ navToggleClose.addEventListener('click' , () => {
     filter.classList.add('hidden');
 
 });
+// smooth scroll for links
+let anchorSelector  = 'a[href^="#"]';
+
+let anchorList = document.querySelectorAll(anchorSelector);
+
+anchorList.forEach(link => {link.onclick = function (e) {
+    e.preventDefault();
+
+    let destination = document.querySelector(this.hash);
+    destination.scrollIntoView({behavior : 'smooth'});
+}})
